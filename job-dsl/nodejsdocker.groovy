2fb77122f9845c590a970fe1274e0d1d466102e6
Gulivetor <51383224+Gulivetor@users.noreply.github.com>
2021-05-15 00:36:37 +0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,9 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        //git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/Gulivetor/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('admin')
+            node / gitConfigEmail('adamjr1083@yahoo.com')
         }
     }
     triggers {
@@ -14,7 +15,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('gullivetor/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
